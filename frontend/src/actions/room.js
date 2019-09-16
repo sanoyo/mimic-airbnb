b4236c270a9b7b4e2fad97b84bc4d82530096999
sanoyo <sa00380304yo@gmail.com>
2019-09-16 22:49:16 +0900

    room action reducer 追加

---
+++ frontend/src/actions/room.js
@@ -0,0 +1,28 @@
+import { HOST } from '../constants';
+import { normalizeRooms } from '../utils';
+
+export const SET_ROOMS = 'SET_ROOMS';
+
+export function setRooms(rooms) {
+  return {
+    type: SET_ROOMS,
+    rooms
+  }
+}
+
+export function getRooms(rooms) {
+ return (dispatch) => {
+   return fetch(`${HOST}/api/v1/rooms`)
+   .then(response => response.json())
+   .then(json => {
+     console.log('getRooms', json);
+
+     if (json.is_success) {
+      dispatch(setRooms(normalizeRooms(json.rooms)));
+     } else {
+       alert(json.error);
+     }
+   })
+   .catch(e => alert(e));
+ }
+}
