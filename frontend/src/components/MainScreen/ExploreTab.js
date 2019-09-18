3620bb6dee2cf12477530db850503270a5af4a59
sanoyo <sa00380304yo@gmail.com>
2019-09-19 07:56:24 +0900

    画像の表示に関して TODO コメント残した

--- frontend/src/components/MainScreen/ExploreTab.js
+++ frontend/src/components/MainScreen/ExploreTab.js
@@ -36,44 +36,7 @@ const styles = StyleSheet.create({
   }
 });
 
-// const items = [
-//   {
-//     id: 1,
-//     title: 'New York',
-//     homeType: 'House',
-//     image: { uri: 'https://design-1st.jp/wp/wp-content/uploads/2018/11/3f8a2c548c51c116fcc79d650c98dd161-e1543190476505.jpg' },
-//     bedRoom: 2,
-//     price: 150,
-//     instant: true
-//   },
-//   {
-//     id: 2,
-//     title: 'Paris',
-//     homeType: 'House',
-//     image: { uri: 'http://livedoor.blogimg.jp/tsukasahome-8343/imgs/6/0/605108d0.jpg' },
-//     bedRoom: 1,
-//     price: 99,
-//     instant: false
-//   },
-//   {
-//     id: 3,
-//     title: 'Melbourne',
-//     homeType: 'Apartment',
-//     image: { uri: 'https://p.limia.jp/resize/o3/image/435/gallery/25266/a2afc58c5ac9c65a661b3c1daeeb7631.jpg' },
-//     bedRoom: 1,
-//     price: 99,
-//     instant: false
-//   },
-// ];
-
 class ExploreTab extends Component {
-
-  // constructor(props) {
-  //   super(props);
-  //   this.state = {
-  //     items: items
-  //   };
-  // }
 
   componentWillMount() {
     this.props.getRooms();
@@ -88,11 +51,12 @@ class ExploreTab extends Component {
     return (
       <FlatList
         style={styles.container}
-        // data={this.state.items}
-        data = { rooms }
+        data={this.state.items}
+        // data = { rooms }
         renderItem={({item}) => 
           <TouchableOpacity onPress={() => this.onPress(item)} style={styles.item}>
-            <Image style={styles.image} source = {{uri: item.image}}/>
+            {/* TODO 画像が表示されないので対応要 */}
+            <Image style={styles.image} source={{uri: item.uri}} />
             <Text style = {styles.title}>{`$${item.price} ${item.instant ? '🎉 ' : ''}${item.title}`}</Text>
             <Text>{`${item.homeType} - ${item.bedRoom} bedroom(s)`}</Text>
           </TouchableOpacity>
